26358ce1d3097d4162681c6ea2097bb24e6e801c
유호진 <hojin.yu@supercat.co.kr>
2023-02-02 18:03:19 +0900

    (end) redis test end

--- main.go
+++ main.go
@@ -7,19 +7,29 @@ import (
 	"github.com/jjimgo/go_gin/models"
 	"github.com/jjimgo/go_gin/services"
 	"github.com/jjimgo/go_gin/controllers"
+	redis "github.com/redis/go-redis/v9"
+)
+
+var (
+	server		*gin.Engine
+	err			error
 )
 
 var (
 	ctx			context.Context
-	server		*gin.Engine
 	us			services.UserService
 	uc			controllers.UserController
-	err			error
 	userList	[]*models.User
 )
 
+var (
+	rctx		context.Context
+	rds 		services.RedisService
+	rdsc		controllers.RedisController
+)
 
 func init() {
+	// ---- user ---
 	ctx = context.TODO()
 
 	userOne := models.User{"testName1",  1}
@@ -29,12 +39,24 @@ func init() {
 	us = services.NewUserService(userList,ctx)
 	uc = controllers.New(us)
 
+	// ---- redis ---
+	rctx = context.Background()
+	rgb := redis.NewClient(&redis.Options{
+		Addr:	  "redis-16990.c289.us-west-1-2.ec2.cloud.redislabs.com:16990",
+		Password: "MpE2LxEUJwWAYdfPV6NUX2S8ywN9Gcjg", 
+		DB:		  0,  
+	})
+
+	rds = services.NewRedisService(rgb, rctx)
+	rdsc = controllers.NewRedisController(rds)
+
 	server = gin.Default()
 }
 
 func main() {
 	basePath := server.Group("/")
 	uc.RegisterUserRoutes(basePath)
+	rdsc.RegisterRedisRouter(basePath)
 
 	log.Fatal(server.Run(":8080"))
 }
